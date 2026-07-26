5ca374ef01774f34f353b499926249f229ee7dd8
bishal magar <bm080458@gmail.com>
2026-07-26 16:30:52 +0545

    User relative api path for production

--- E-commerce/src/Config/apiConfig.js
+++ E-commerce/src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454/api";
+export const API_BASE_URL = "/api";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
